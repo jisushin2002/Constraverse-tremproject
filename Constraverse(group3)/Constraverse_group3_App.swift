617cbf4cc74faf5b6be197551578c06999d6b662
Jisu <jshin21@yonsei.ac.kr>
2023-06-07 20:20:21 +0900

    Initial Commit

---
+++ Constraverse(group3)/Constraverse_group3_App.swift
@@ -0,0 +1,17 @@
+//
+//  Constraverse_group3_App.swift
+//  Constraverse(group3)
+//
+//  Created by snlcom on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct Constraverse_group3_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
